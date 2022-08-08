da7eed69160db3cfc73a30fe9a43d1166628cb8f
Remix Workspace <>
2022-08-08 01:05:08 -0700

    default values

---
+++ contracts/defaultvalues.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+contract defaultValues{
+    bool public b;//default value is false
+    uint public u;//0
+    int public i;//0
+    address public a;//sequence of 40 zeroes
+    bytes32 public b32;//32 hexadecimal of 64 zeroes;
+
+}
